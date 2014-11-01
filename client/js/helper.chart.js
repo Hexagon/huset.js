771a0edbb31ebd0cfc7690aa6da6d824a743da98
Hexagon <robinnilsson@gmail.com>
2014-11-01 19:38:51 +0100

    Converting to grapho, adding temperature graphs

--- client/js/helper.chart.js
+++ client/js/helper.chart.js
@@ -1,12 +1,17 @@
-  var parseDateT = d3.time.format("%Y-%m-%dT%H:%M:%S").parse;
-  var currentGraph = []; 
+  var currentGraph;
 
   // Function to generate charts
-  function gen_chart_json(dest,d1,title,prop1,prop2,axis) {
-    var data = [],i;
+  function gen_chart_json(dest,d1,title,prop1,prop2,series,axis) {
+    if ( currentGraph == undefined ) currentGraph = [];
+    var data = [],i,currentGraph;
     if (prop1 !== undefined) {
       for(i = 0; i<d1.length; i++) {
-        data.push([d1[prop1],d1[prop_2]]);
+	var di=Date.parse(d1[i][prop1]);
+	if ( !isNaN(di) ) {
+	        data.push([di/1000,d1[i][prop2]]);
+	} else {
+	        data.push([d1[i][prop1],d1[i][prop2]]);
+	}
       }
     } else {
       data = d1;
@@ -15,25 +20,25 @@
       grapho.removeDataset(currentGraph[dest]);
       currentGraph[dest] = undefined;
     }
+console.log(data);
     currentGraph[dest] = grapho.addDataSet({
         type:'line',
         strokeStyle: '#F0F066',
+	lineSmooth: false,
+	name: series,
         y: { 
             axis: axis,
             scaleStyle: '#F0F066',
-            showGridLines: true,
+            showGridLines: (axis===1) ? true : false,
             name: title,
-            labelFont: '11px Droid Sans',
-            nameFont: '20px Droid Sans',
-            labelStyle: '#FF0000',
             labels: true
         },
         x: { 
             labels: true,
-            labelFormat: Grapho.formats.time
+            labelFormat: Grapho.formats.time,
+	    labelRotation: 30
         },
         data: data
     });
-
   }
 
