d5d3863bd6554fb111582390cdf00cba0a61ac0f
Hexagon <robinnilsson@gmail.com>
2014-11-01 18:08:24 +0100

    Test

--- client/js/helper.chart.js
+++ client/js/helper.chart.js
@@ -2,9 +2,19 @@
   var currentGraph = []; 
 
   // Function to generate charts
-  function gen_chart_json(dest,d1,title,axis) {
-    console.log(d1);
-    grapho.removeDataset(currentGraph[dest]);
+  function gen_chart_json(dest,d1,title,prop1,prop2,axis) {
+    var data = [],i;
+    if (prop1 !== undefined) {
+      for(i = 0; i<d1.length; i++) {
+        data.push([d1[prop1],d1[prop_2]]);
+      }
+    } else {
+      data = d1;
+    }
+    if (currentGraph !== undefined && currentGraph[dest]) {
+      grapho.removeDataset(currentGraph[dest]);
+      currentGraph[dest] = undefined;
+    }
     currentGraph[dest] = grapho.addDataSet({
         type:'line',
         strokeStyle: '#F0F066',
@@ -22,7 +32,7 @@
             labels: true,
             labelFormat: Grapho.formats.time
         },
-        data: d1
+        data: data
     });
 
   }
